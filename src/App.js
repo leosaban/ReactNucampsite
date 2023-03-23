08ea815aa84af1b5726f97e68a840effbdb23b3b
Leo Saban <leodsaban@gmail.com>
2023-03-23 12:10:45 -0600

    HomePage Display

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png'
+import { Routes, Route } from 'react-router-dom'
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <Container>
-                <NavbarBrand href='/'>
-                    <img src={NucampLogo} alt='nucamp logo'/>
-                </NavbarBrand>
-            </Container>
-        </Navbar>
-        I'm ready for workshop!
+    <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
+    <Footer />
     </div>
   );
 }
