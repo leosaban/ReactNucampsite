d9e55ab4e3d94483587802469f185ad342cff1cb
Leo Saban <leodsaban@gmail.com>
2023-03-30 12:34:58 -0600

    validateContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
-import {Formik, Field, Form} from 'formik';
+import {Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateContactForm } from "../utils/validateContactForm";
 
 const ContactForm = () => {
     const handleSubmit = (values, {resetForm}) => {
@@ -17,7 +18,9 @@ const ContactForm = () => {
             contactType:'By Phone',
             feedback:''
         }}
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit}
+        validate ={validateContactForm}
+>
         <Form>
             <FormGroup row>
                 <Label htmlFor="firstName" md='2'>
@@ -28,6 +31,9 @@ const ContactForm = () => {
                         name='firstName'
                         placeholder='First Name'
                         className='form-control'/>
+                    <ErrorMessage name='firstName' >
+                        {(msg) => <p className="text-danger">{msg}</p>}    
+                    </ErrorMessage>
                 </Col>
             </FormGroup>
 
@@ -40,6 +46,9 @@ const ContactForm = () => {
                     name='lastName'
                     placeholder='Last Name'
                     className='form-control'/>
+                    <ErrorMessage name='lastName' >
+                        {(msg) => <p className="text-danger">{msg}</p>}    
+                    </ErrorMessage>    
                 </Col>
             </FormGroup>
 
@@ -52,6 +61,9 @@ const ContactForm = () => {
                     name='phoneNum'
                     placeholder='Phone'
                     className='form-control'/>
+                    <ErrorMessage name='phoneNum' >
+                        {(msg) => <p className="text-danger">{msg}</p>}    
+                    </ErrorMessage>    
                 </Col>
             </FormGroup>
             
@@ -65,6 +77,9 @@ const ContactForm = () => {
                     placeholder='Email'
                     type='email'
                     className='form-control'/>
+                    <ErrorMessage name='email' >
+                        {(msg) => <p className="text-danger">{msg}</p>}    
+                    </ErrorMessage>    
                 </Col>
             </FormGroup>
 
