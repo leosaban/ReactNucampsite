465d5b2c39eca0a91d5a12f8fc0a9f9f9d22f5f3
Leo Saban <leodsaban@gmail.com>
2023-04-06 11:03:14 -0600

    Redux for All

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
@@ -5,10 +6,10 @@ import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommensList';
 import SubHeader from '../components/SubHeader';
 
-
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
-    const campsite = selectCampsiteById(campsiteId);
+    const campsite = useSelector(selectCampsiteById(campsiteId));
+    console.log('campsite', campsite);
 
     return (
         <Container>
